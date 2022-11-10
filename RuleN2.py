3921b90d74f2b02210b05e0df753b3c9cfebff8b
Rick Howell <88335430+barthSextic@users.noreply.github.com>
2022-11-09 21:25:00 -0500

    Need to add input validation, and a path to output file.

--- RuleN2.py
+++ RuleN2.py
@@ -1,3 +1,13 @@
+"""
+Rick Howell
+Rule N Cellular Automata Generator
+
+v 0.9.0
+Spits out a file named 'output.png' in the source folder
+There is no input validation
+    Do not try to limit test this, the program will crash and burn
+"""
+
 import random
 import sys
 from tkinter import *
@@ -35,7 +45,7 @@ ruleSet(ruleSize, 8, rules)
 ruleSet(8, 3, condition)
 
 
-def buildSeed(width):
+def buildSeed(width, n, b):
     # make middle seed
     buffDumpMid = []
     halfway = (width - 1) // 2
@@ -49,6 +59,13 @@ def buildSeed(width):
         buffDumpRand.insert(j, random.randint(0, 1))
         seed.insert(1, buffDumpRand)
     # make modular seed
+    buffDumpMod = []
+    for k in range(width):
+        if k % n == 0:
+            buffDumpMod.insert(k, 1)
+        else:
+            buffDumpMod.insert(k, 0)
+    seed.insert(2, buffDumpMod)
 
 
 def buildImage(rSet, seedRow, column, row):
@@ -85,52 +102,81 @@ def buildImage(rSet, seedRow, column, row):
 root = Tk()
 
 root.title("Rick Howell Rule N")
+root.geometry("360x300")
 
-textBoxW = 20
+textBoxW = 10
+textBoxWSmall = 3
 borderW = 5
 
 # get width
 w = Entry(root, width=textBoxW, borderwidth=borderW)
-w.insert(0, "1920")
+w.insert(0, 121)
 
 # get height
 h = Entry(root, width=textBoxW, borderwidth=borderW)
-h.insert(0, "1080")
+h.insert(0, 121)
 
 # get rule [0, 255]
-r = Entry(root, width=3, borderwidth=borderW)
-r.insert(0, "73")
+r = Entry(root, width=textBoxWSmall, borderwidth=borderW)
+r.insert(0, 30)
+
+# a ~ b (mod n)
+b = Entry(root, width=textBoxWSmall, borderwidth=borderW)
+b.insert(0, 0)
+n = Entry(root, width=textBoxWSmall, borderwidth=borderW)
+n.insert(0, 12)
+
+# get seed [0, 3]
+seedSelect = IntVar()
+s1 = Radiobutton(root, text="Middle Start", variable=seedSelect, value=0)
+s2 = Radiobutton(root, text="Random Start", variable=seedSelect, value=1)
+s3 = Radiobutton(root, text="Modular Start", variable=seedSelect, value=2)
 
 
 def clickRender():
     rule = rules[int(r.get())]
     width = int(w.get())
     height = int(h.get())
-    buildSeed(width)
-    imageArray = np.array(buildImage(rule, seed[0], width, height))
+    # width, n, b
+    buildSeed(width, int(n.get()), int(b.get()))
+    s = seedSelect.get()
+    # Build image with information
+    imageArray = np.array(buildImage(rule, seed[s], width, height))
     for x in range(len(imageArray)):
         for y in range(len(imageArray[x])):
             imageArray[x][y] *= 255
-    cv.imwrite("test.png", imageArray)
+    cv.imwrite("output.png", imageArray)
     sys.exit()
 
 
 # make graphics objects
-txtInfo = Label(root, text="_____")
+txtInfo = Label(root, text="Rule [0, 255]: ")
 txtWidth = Label(root, text="Width: ")
 txtHeight = Label(root, text="Height: ")
+txtSeed = Label(root, text="Select Seed")
+txtModular = Label(root, text="a ~ b (mod n)")
+txtB = Label
 
 btnRender = Button(root, text="Render", command=clickRender, padx=50, pady=20, fg="red")
 
 # put objects on the screen
-txtInfo.grid(row=0, column=2)
-txtWidth.grid(row=1, column=0)
-txtHeight.grid(row=2, column=0)
+txtInfo.grid(row=0, column=0)
+txtWidth.grid(row=7, column=0)
+txtHeight.grid(row=8, column=0)
+txtModular.grid(row=1, column=1)
 
-btnRender.grid(row=3, column=0)
+btnRender.grid(row=10, column=0)
 
-w.grid(row=1, column=1)
-h.grid(row=2, column=1)
-r.grid(row=0, column=0)
+w.grid(row=7, column=1)
+h.grid(row=8, column=1)
+# rule info
+r.grid(row=0, column=1)
+
+s1.grid(row=1, column=0)
+s2.grid(row=2, column=0)
+s3.grid(row=3, column=0)
+
+b.grid(row=2, column=1)
+n.grid(row=3, column=1)
 
 root.mainloop()
